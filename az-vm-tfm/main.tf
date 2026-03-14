f80f325b6184bd1657bf59c9ddab93baaa6ad3f9
psamardzhiev <petko9001@gmail.com>
2026-03-14 19:33:23 +0200

    added additional dependencies between the tf resources in main.tf file to optimize the terraform

--- az-vm-tfm/main.tf
+++ az-vm-tfm/main.tf
@@ -38,7 +38,10 @@ resource "azurerm_subnet" "internal" {
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
-   depends_on = [ azurerm_resource_group.main ]
+   depends_on = [ 
+    azurerm_resource_group.main,
+    azurerm_virtual_network.vnet 
+    ]
 }
 
 # 4. Public IP (So you can access the VM)
